0d248087a42ad9e80c6427c3c042b5bd9a60d71c
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-16 15:17:53 +0530

    variable

---
+++ 3_java/variables.java
@@ -0,0 +1,10 @@
+public class variables
+{
+    public static void main(String[] args) {
+        int number=900;
+        int younumber;
+        System.out.println(number);
+        younumber=899;
+        System.out.println(younumber);
+    }
+}
