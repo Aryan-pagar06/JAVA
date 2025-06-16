025ee025ae6a7bbb65599e6963e0c5842e7881b1
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-16 14:07:32 +0530

    third

---
+++ 2_java_basic/Patterns/third.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+public class third
+{
+public static void main (String [] args)
+{
+System.out.println("   *");
+System.out.println("  **");
+System.out.println(" ***");
+System.out.println("****");
+}
+}
