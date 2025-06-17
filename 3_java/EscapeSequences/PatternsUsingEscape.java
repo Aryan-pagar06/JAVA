253bf82fb05f4675f5a51036107a819107f41ca3
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-17 11:37:15 +0530

    patternsUsingEscape

---
+++ 3_java/EscapeSequences/PatternsUsingEscape.java
@@ -0,0 +1,11 @@
+public class PatternsUsingEscape 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("*\n**\n***\n****\n*****");
+        System.out.println("\n");
+        System.out.println("*****\n****\n***\n**\n*");
+        System.out.println("\n");
+        System.out.println("    *\n   **\n  ***\n ****\n*****");
+    }
+}
