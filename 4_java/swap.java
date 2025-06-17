711bc628deb93b724bfc4aefff13530380ab508d
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-17 13:08:29 +0530

    swap

---
+++ 4_java/swap.java
@@ -0,0 +1,15 @@
+public class swap
+{
+    public static void main (String[]args)
+    {
+        int a=5;
+        System.out.println(a);
+        int b=10;
+        System.out.println(b);
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
