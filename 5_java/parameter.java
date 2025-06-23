3f6abc5ab2d4b09c26aa080bd4694f9307b71ff9
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-23 13:08:04 +0530

    add

---
+++ 5_java/parameter.java
@@ -0,0 +1,12 @@
+public class parameter
+{
+    public static void main(String[] args) 
+    {
+       int sum= addition (45, 45);
+       System.out.println(sum);
+    }
+    public static int addition(int a, int b)
+    {
+        return a+b;
+    }
+}
