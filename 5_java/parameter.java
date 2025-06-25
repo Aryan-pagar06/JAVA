d83bd26ce801ca93539cdc35e472e0807e6a9af8
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-25 15:27:47 +0530

    new

--- 5_java/parameter.java
+++ 5_java/parameter.java
@@ -1,8 +1,14 @@
+import java.util.*;
 public class parameter
 {
     public static void main(String[] args) 
     {
-       int sum= addition (45, 45);
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter first number = ");
+       int num1=input.nextInt();
+       System.out.print("\nEnter second number = ");
+       int num2=input.nextInt();
+       int sum= addition (num1, num2);
        System.out.println(sum);
     }
     public static int addition(int a, int b)
