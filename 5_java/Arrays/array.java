cd1dfeb2b47c2b7bf2448b6c1b28a305a1fef17b
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-09-07 00:17:14 +0530

    Sum and average of the array

---
+++ 5_java/Arrays/array.java
@@ -0,0 +1,13 @@
+public class array
+{
+    public static void main(String[] args) 
+    {
+    int [] myarray = {1,2,3,4,5};
+    int i=0;
+    while(i<myarray.length)
+    {
+    System.out.print(myarray[i]);
+    i++;
+    }
+    }
+}
