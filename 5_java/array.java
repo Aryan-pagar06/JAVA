d1864e09c60473f700073f5c3ae93dcfb39a4995
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-09-06 18:51:30 +0530

    Arrays

--- 5_java/array.java
+++ 5_java/array.java
@@ -1,5 +1,13 @@
-import java.util.*;
 public class array
 {
-    
+    public static void main(String[] args) 
+    {
+    int [] myarray = {1,2,3,4,5};
+    int i=0;
+    while(i<myarray.length)
+    {
+    System.out.print(myarray[i]);
+    i++;
+    }
+    }
 }
