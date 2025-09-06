587bbc4c3c85c867471d0aad6640ee895ac3fac5
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-09-06 15:00:34 +0530

    Arrays

--- 5_java/reverse.java
+++ 5_java/reverse.java
@@ -7,7 +7,9 @@ public class reverse
     int num=input.nextInt();
     while (num>=1){
     int rem=num%10; //1234%10=4 , 123%10=3 , 12%10=2, 1%10=1
+    
     System.out.print(rem);  //4321
+    
     num=num/10; //num=1234/10=123, num=123/10=12, num=12/10=1, num=1/10
     
     }
