0616a1cbaf932e2aa7d067bbc3a0973849e40fc4
Aryan-pagar06 <aryanpagar34@gmail.com>
2026-01-07 20:20:08 +0530

    Remove compiled .class files and old Java files

--- 2_java_basic/goodmorning.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-public class goodmorning
-{
-public static void main (String [] args)
-{
-System.out.print("good morning");
-}
-}
--- 2_java_basic/subscribe.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-public class subscribe
-{
-public static void main (String [] args)
-{
-System.out.print("subscribe");
-}
-}
