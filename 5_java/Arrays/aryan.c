060ed3949509dba19c83a6ff4a694e90368d7ba3
Aryan-pagar06 <aryanpagar34@gmail.com>
2026-01-07 20:11:12 +0530

    changes

---
+++ 5_java/Arrays/aryan.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <graphics.h>
+
+int main() {
+    // Write C code here
+    printf("Try programiz.pro");
+
+    return 0;
+}
